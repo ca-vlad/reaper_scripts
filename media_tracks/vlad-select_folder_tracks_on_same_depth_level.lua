49315aa7d0e121db15b080db3fc90649e00ea3e9
ca-vlad <caciulavlad@gmail.com>
2022-05-09 13:25:14 +0200

    Add documentation and rename the script

--- media_tracks/vlad-select_folder_tracks_on_same_depth_level.lua
+++ media_tracks/vlad-select_folder_tracks_on_same_depth_level.lua
@@ -1,22 +1,23 @@
 --[[
-  * ReaScript Name:
+  * ReaScript Name: Select folder tracks on same depth level
   * About:
-
+      Adds all folder tracks on the same depth level, from the entire current project, to the selection.
   * Instructions:
+      Select one single folder track and run the script.
 
   * Author: ca-vlad
   * Repository: GitHub > ca-vlad > reaper_scripts
   * Repository URI: https://github.com/ca-vlad/reaper_scripts
   * Licence: GPL v3
   * Version: 1.0
-  * Version Date:
+  * Version Date: 2022-05-09
   * REAPER: 6.56
   * Extensions:
 --]]
 
 --[[
 * Changelog:
-* v1.0 (2022-01-25)
+* v1.0 (2022-05-09)
 + Initial Release
 --]]
 
@@ -36,18 +37,18 @@ function main()
     return
   end
 
-  -- TODO: Add check for folder track
-
-  -- Get original track depth
   local original_track = reaper.GetSelectedTrack(0, 0)
-
   local original_track_depth = reaper.GetTrackDepth(original_track)
-  -- local original_track_folder_depth = reaper.GetMediaTrackInfo_Value(original_track, 'I_FOLDERDEPTH')
-  -- local track_level = reaper.GetMediaTrackInfo_Value(original_track, 'P_PARTRACK')
+  local original_track_folder_depth = reaper.GetMediaTrackInfo_Value(original_track, 'I_FOLDERDEPTH')
+
+  if original_track_folder_depth ~= 1.0 then
+    reaper.ShowMessageBox("Error: Please select one folder track", "Error", 0)
+    return
+  end
 
   -- msg("Track depth is " .. original_track_depth .. " folder depth is " .. original_track_folder_depth)
 
-  i = 0
+  local i = 0
   while i < reaper.CountTracks(0) do
     local track = reaper.GetTrack(0, i)
 
@@ -61,7 +62,7 @@ function main()
 
   reaper.UpdateArrange()
 
-  reaper.Undo_EndBlock("Select level one tracks", -1)
+  reaper.Undo_EndBlock("Select folder tracks on same depth level", -1)
 
 end
 
